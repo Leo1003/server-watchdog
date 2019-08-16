0aff5f38e91879992cab8ce16ac32e04f7cac790
Leo <leo881003@gmail.com>
2019-08-16 15:11:42 +0800

    Basic settings and add server api

---
+++ api/server/controllers/Server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+* Read the documentation () to implement custom controller functions
+*/
+
+module.exports = {
+    /**
+     * Retrieve records.
+     *
+     * @return {Array}
+     */
+
+    async find(ctx) {
+        console.log(strapi.services.server);
+        let query;
+        if (ctx.query._q) {
+            query = await strapi.services.server.search(ctx.query);
+        } else {
+            query = await strapi.services.server.find(ctx.query);
+        }
+        return strapi.services.server.filter(ctx.state.user, query);
+    },
+
+    /**
+     * Promise to fetch record
+     *
+     * @return {Promise}
+     */
+
+    async findOne(ctx) {
+        let query = await strapi.services.server.findOne(ctx.params);
+        return strapi.services.server.filter(ctx.state.user, query);
+    },
+};
---
+++ api/server/services/Server.js
@@ -0,0 +1,24 @@
+'use strict';
+const _ = require('lodash');
+
+/**
+* Read the documentation () to implement custom service functions
+*/
+
+module.exports = {
+    filter(user, query) {
+        function _filter(i) {
+            return _.pick(i, ['_id', 'name', 'lastPing', 'lastSocketPing']);
+        }
+
+        if (!user) {
+            if (Array.isArray(query)) {
+                return _.map(query, _filter);
+            } else {
+                return _filter(query);
+            }
+        } else {
+            return query;
+        }
+    }
+};
