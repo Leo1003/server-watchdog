10c4471550e2531494c22324fc9ada53e8ecca8d
Leo <leo881003@gmail.com>
2019-09-03 23:20:20 +0800

    Integrate tgbot with strapi

---
+++ hooks/tgbot/index.js
@@ -0,0 +1,31 @@
+'use strict';
+const _ = require('lodash');
+const TGBot = require('node-telegram-bot-api');
+
+module.exports = strapi => {
+  const hook = {
+
+    /**
+     * Default options
+     */
+
+    defaults: {
+      token: "",
+      gid: ""
+    },
+
+    /**
+     * Initialize the hook
+     */
+
+    initialize: cb => {
+      strapi.tgbot = new TGBot(strapi.config.hook.settings.tgbot['token'], { polling: false });
+      cb();
+    },
+    sendMessage: (msg) => {
+      strapi.tgbot.sendMessage(strapi.config.hook.settings.tgbot['gid'], msg);
+    }
+  };
+
+  return hook;
+};
